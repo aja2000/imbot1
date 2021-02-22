0e83f4c5f644f9d9cbc041311f4d43da70240188
Avery <averyjames1220@gmail.com>
2021-02-21 22:54:55 -0600

    added main

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+
+file = open("key.text", "r")
+key = file.read()
+
+
+class Client(discord.Client):
+    def __init__(self):
+        super().__init__()
+
+    async def on_ready(self):
+        print("Rewddy")
+
+    # we are now connected to discord's API
+    async def on_message(self, message):
+        if message.guild.name == "Test" and str(message.content).lower().__contains__("poop"):
+            print(message.content, message.author)
+            await message.channel.send("Die")
+
+
+client = Client()
+client.run(key)
